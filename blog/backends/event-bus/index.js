f4069aca2c9e3399d0eaaec173a13a8349a4cb0e
Chris Tseng <samfire5200@gmail.com>
2022-05-29 23:13:48 +0800

    02-36. Creating the Data Query Service

--- blog/backends/event-bus/index.js
+++ blog/backends/event-bus/index.js
@@ -8,12 +8,15 @@ app.use(bodyParser.json());
 app.post("/events", (req, res) => {
   const event = req.body;
 
+  // Posts service
   axios.post("http://localhost:4000/events", event).catch((err) => {
     console.log(err.message);
   });
+  // Comments service
   axios.post("http://localhost:4001/events", event).catch((err) => {
     console.log(err.message);
   });
+  // Query service
   axios.post("http://localhost:4002/events", event).catch((err) => {
     console.log(err.message);
   });
