29344347c7737973a00fa1037952069680cc7ef7
Chris Tseng <samfire5200@gmail.com>
2022-05-30 14:24:11 +0800

    02-37. Parsing Incoming Events

--- blog/backends/query/index.js
+++ blog/backends/query/index.js
@@ -6,10 +6,34 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/posts', (req, res) => {});
+const posts = {};
+
+app.get('/posts', (req, res) => {
+  res.send(posts);
+});
 
 app.post('/events', (req, res) => {
-  console.log("Event Received", req.body.type);
+  // console.log("Event Received", req.body.type);
+  const { type, data } = req.body;
+
+  if (type === 'PostCreated') {
+    const { id, title } = data;
+    posts[id] = { id, title, comments: [], type };
+  }
+
+  if (type === 'CommentCreated') {
+    const { id, content, postId } = data;
+    const post = posts[postId];
+    if (post !== undefined) {
+      post.comments.push({ id, content, type });
+      // console.log(posts);
+    } else {
+      console.log('Invalid Post, need further development here...')
+      // get Post here ...
+    }  
+  }
+
+  res.send({});
 });
 
 app.listen(4002, () => {
