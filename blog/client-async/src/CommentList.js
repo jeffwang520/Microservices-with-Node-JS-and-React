fe0d8b6afd7bde00a63195e1596a1e355ecd83d6
Chris Tseng <samfire5200@gmail.com>
2022-06-04 15:20:11 +0800

    02-Dealing with Missing Events

--- blog/client-async/src/CommentList.js
+++ blog/client-async/src/CommentList.js
@@ -1,6 +1,21 @@
+
 const CommentList = ({ comments }) => {
   const renderedComments = comments.map((comment) => {
-    return <li key={comment.id}>{comment.content}</li>;
+    let content;
+
+    if (comment.status === "approved") {
+      content = comment.content;
+    }
+
+    if (comment.status === "pending") {
+      content = comment.content + ' (pending)';
+    }
+
+    if (comment.status === "rejected") {
+      content = comment.content + ' (rejected)';
+    }
+
+    return <li key={comment.id}>{content}</li>;
   });
 
   return <ul>{renderedComments}</ul>;
